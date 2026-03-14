ff8f3e2b0255f28299d78ea62c99c2374bc8a32c
SanapathiNarendra <sanapathinarendra99@gmail.com>
2026-03-14 20:50:22 +0530

    Feat(WaterBottle) : WaterBottle calculation

---
+++ WaterBottle.js
@@ -0,0 +1,7 @@
+
+function calculateBottles() {
+  debugger;
+    var totalWater = document.getElementById("totalWater").value;
+    var result = totalWater / 1.5;
+    document.getElementById("Output").innerHTML=result
+}
